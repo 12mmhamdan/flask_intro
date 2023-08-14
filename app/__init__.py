eca00a0d96e1c89461ea9979c2d7aa68edc41390
Moataz Hamdan <hamdanmoataz@gmail.com>
2023-08-14 15:59:36 -0500

    fixed init py

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 # import all of the routes file into the current package
 from app import routes
-# must be imported at the bottom of the fil
+# must be imported at the bottom of the file
