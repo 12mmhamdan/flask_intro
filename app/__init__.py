d625e8f99d93150eaa1b48a6f073831f4c971cda
Moataz Hamdan <hamdanmoataz@gmail.com>
2023-08-15 15:14:14 -0500

    configuration for Flask-SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 # Create an instance of the flask application 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hello'
+
+# configure our app with the values from the Config class
+app.config.from_object(Config)
+
+# create an instance of SQLALCHEMY to represent our db
+db = SQLAlchemy(app)
+
+# create an instance of Migrate to handle the database migrations of our flask app
+migrate = Migrate(app, db)
+
 
 # import all of the routes file into the current package
 from app import routes
