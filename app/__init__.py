8b53dd9132718532cc671ed06c17340320809bf8
Moataz Hamdan <hamdanmoataz@gmail.com>
2023-08-15 12:11:23 -0500

    changed layout of index.html, added a new requirement to requirement.txt, anded form.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 # Create an instance of the flask application 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hello'
 
 # import all of the routes file into the current package
 from app import routes
