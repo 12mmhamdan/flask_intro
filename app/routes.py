41f8728efdbba1a5885303ffa5814949f908b1ab
Moataz Hamdan <hamdanmoataz@gmail.com>
2023-08-15 14:08:31 -0500

    add post method to return back to main website after fom validation completed

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,23 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, redirect
 from app.forms import SignUpForm
 # Add a route
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     form = SignUpForm()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        username = form.username.data
+        email= form.email.data
+        password=form.password.data
+        print(first_name, last_name, username, email, password)
+        return redirect(url_for('index'))
+    else:
+        print('wrong password dummy')
     return render_template('signup.html', form=form)
 
