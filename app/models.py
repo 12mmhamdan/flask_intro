1f913936b19e879a984e53b3b38b505f2339d619
Moataz Hamdan <hamdanmoataz@gmail.com>
2023-08-16 12:12:26 -0500

    Created post table in db

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from random import randint
 
 # DDL Statement
 # Create TABLE user(
@@ -28,3 +28,21 @@ class User(db.Model):
     
     def check_password(self, password_guess):
         return check_password_hash(self.password, password_guess)
+def random_photo():
+    return f"https://picsum.photos/500?random={randint(1,100)}"
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String, nullable=False, default = random_photo)
+    date_created=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+
+    def __repr__(self):
+        return f"<Post {self.id}|{self.title}>"
+    
+   
