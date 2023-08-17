d16a74c6861883a5fd07648ef742fee3e12cb6c2
Moataz Hamdan <hamdanmoataz@gmail.com>
2023-08-17 13:41:37 -0500

    get post by id endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
 # Create an instance of the flask application 
 app = Flask(__name__)
 
@@ -17,10 +18,14 @@ migrate = Migrate(app, db)
 
 # create an instance of login manager to handle authenitcation
 login = LoginManager(app)
-
+# Customize login process
 login.login_view = 'login'
 login.login_message='You need to be logged in for that that!'
 login.login_message_category = 'info'
+
+# register api blueprint with our app
+from app.blueprints.api import api
+app.register_blueprint(api)
 
 # import all of the routes file into the current package
 from app import routes,models
